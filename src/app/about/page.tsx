89578cee1345169b668e0b66ed25d16cf1b4519d
André Luiz <andreluizoliveira163@hotmail.com>
2025-12-16 14:32:00 -0400

    Sobre layout.tsx (root) e Metadata do Next para title e description

---
+++ src/app/about/page.tsx
@@ -0,0 +1,17 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "About",
+};
+
+export default function AboutPage() {
+    return (
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate
+            sequi dolorem excepturi rerum provident, incidunt omnis
+            necessitatibus quod blanditiis aliquid repudiandae beatae
+            praesentium voluptatibus neque, fugiat, minima commodi vel
+            obcaecati.
+        </p>
+    );
+}
