60954e5829c1e7dca9164f9fe099a0cdd0545a88
André Luiz <andreluizoliveira163@hotmail.com>
2025-12-28 18:58:12 -0400

    Componente para as mensagens de erro para o cliente (ErrorMessage)

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,25 +1,12 @@
-import clsx from "clsx";
+import ErrorMessage from "@/components/ErrorMessage";
 
 export default function NotFound() {
     return (
-        <>
-            <title>Página não encontrada</title>
-            <div
-                className={clsx(
-                    "min-h-[350px] bg-slate-900 text-slate-100",
-                    "mb-16 p-8 rounded-xl",
-                    "flex items-center justify-center",
-                    "text-center"
-                )}
-            >
-                <div>
-                    <h1 className="text-7xl/tight mb-4 font-extrabold">404</h1>
-                    <p>
-                        Erro 404 - A página que você está tentando acessa não
-                        existe nesse site.
-                    </p>
-                </div>
-            </div>
-        </>
+        <ErrorMessage
+            pageTitle="Página não encontrada"
+            contentTitle="404"
+            content="Erro 404 - A página que você está tentando acessa não
+                        existe nesse site."
+        />
     );
 }
