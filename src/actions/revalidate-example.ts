0d4c8d644ca937e154fd742c6e6e727bbaacee1d
André Luiz <andreluizoliveira163@hotmail.com>
2026-01-15 14:39:44 -0400

    Usando revalidatePath para revalidar o cache manualmente

--- src/actions/revalidate-example.ts
+++ src/actions/revalidate-example.ts
@@ -1,6 +1,10 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
+
 export async function RevalidateExampleAction(formData: FormData) {
     const path = formData.get("path") || "s";
     console.log("Estou em uma Server Action", path);
+
+    revalidatePath(`${path}`);
 }
