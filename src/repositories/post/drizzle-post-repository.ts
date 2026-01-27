2f2947c72db010788b64753572d3be78b491a93c
André Luiz <andreluizoliveira163@hotmail.com>
2026-01-27 08:57:48 -0400

    Comportamento do Suspense e Loading com e sem Cache no Next.js

--- src/repositories/post/drizzle-post-repository.ts
+++ src/repositories/post/drizzle-post-repository.ts
@@ -2,10 +2,14 @@ import { PostModel } from "@/models/post/post-model";
 import { PostRepository } from "./post-repository";
 import { drizzleDb } from "@/db/drizzle";
 import { logColor } from "@/utils/log-color";
+import { asyncDelay } from "@/utils/async-delay";
+import { SIMULATE_WAIT_IN_MS } from "@/lib/post/constants";
 
 export class DrizzlePostRepository implements PostRepository {
     async findAllPublic(): Promise<PostModel[]> {
+        await asyncDelay(SIMULATE_WAIT_IN_MS, true);
         logColor("Drizzle - findAllPublic", Date.now());
+
         const posts = await drizzleDb.query.posts.findMany({
             orderBy: (posts, { desc }) => desc(posts.createdAt),
             where: (posts, { eq }) => eq(posts.published, true),
@@ -15,7 +19,9 @@ export class DrizzlePostRepository implements PostRepository {
     }
 
     async findBySlugPublic(slug: string): Promise<PostModel> {
+        await asyncDelay(SIMULATE_WAIT_IN_MS, true);
         logColor("Drizzle - findBySlugPublic", Date.now());
+
         const post = await drizzleDb.query.posts.findFirst({
             where: (posts, { eq, and }) =>
                 and(eq(posts.slug, slug), eq(posts.published, true)),
@@ -27,7 +33,9 @@ export class DrizzlePostRepository implements PostRepository {
     }
 
     async findAll(): Promise<PostModel[]> {
+        await asyncDelay(SIMULATE_WAIT_IN_MS, true);
         logColor("Drizzle - findAll", Date.now());
+
         const posts = await drizzleDb.query.posts.findMany({
             orderBy: (posts, { desc }) => desc(posts.createdAt),
         });
@@ -36,7 +44,9 @@ export class DrizzlePostRepository implements PostRepository {
     }
 
     async findById(id: string): Promise<PostModel> {
+        await asyncDelay(SIMULATE_WAIT_IN_MS, true);
         logColor("Drizzle - findById", Date.now());
+
         const post = await drizzleDb.query.posts.findFirst({
             where: (post, { eq }) => eq(post.id, id),
         });
--- src/repositories/post/json-post-repository.ts
+++ src/repositories/post/json-post-repository.ts
@@ -2,6 +2,7 @@ import { PostModel } from "@/models/post/post-model";
 import { PostRepository } from "./post-repository";
 import { resolve } from "path";
 import { readFile } from "fs/promises";
+import { SIMULATE_WAIT_IN_MS } from "@/lib/post/constants";
 
 const ROOT_DIR = process.cwd();
 const JSON_POSTS_FILE_PATH = resolve(
@@ -9,17 +10,15 @@ const JSON_POSTS_FILE_PATH = resolve(
     "src",
     "db",
     "seed",
-    "posts.json"
+    "posts.json",
 );
-
-const SIMULATE_WAIT_IN_MS = 0;
 
 export class JsonPostRepository implements PostRepository {
     private async simulateWait() {
         if (SIMULATE_WAIT_IN_MS <= 0) return;
 
         await new Promise((resolve) =>
-            setTimeout(resolve, SIMULATE_WAIT_IN_MS)
+            setTimeout(resolve, SIMULATE_WAIT_IN_MS),
         );
     }
 
