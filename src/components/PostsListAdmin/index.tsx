4481e2dfab3bebb9c49ee8f896b02219aecc94dc
André Luiz <andreluizoliveira163@hotmail.com>
2026-01-30 22:37:17 -0400

    Dialog: adicionando as funcionalidades do componente

--- src/components/PostsListAdmin/index.tsx
+++ src/components/PostsListAdmin/index.tsx
@@ -32,53 +32,6 @@ export default async function PostListAdmin() {
                     </div>
                 );
             })}
-
-            <div
-                className={clsx(
-                    "fixed z-50 inset-0 bg-black/50 backdrop-blur-xs",
-                    "flex items-center justify-center text-center",
-                )}
-            >
-                <div
-                    className={clsx(
-                        "bg-slate-100 p-6 rounded-lg max-w-2xl mx-6",
-                        "flex flex-col gap-6",
-                        "shadow-lg shadow-black/30",
-                    )}
-                >
-                    <h3 className="text-xl font-extrabold">
-                        Título do diálogo
-                    </h3>
-                    <p>
-                        Lorem, ipsum dolor sit amet consectetur adipisicing
-                        elit. Quos nesciunt nihil voluptatem eligendi labore
-                        asperiores eum dicta officia. Recusandae in maiores
-                        quidem mollitia quaerat aperiam sunt illo minima dolor
-                        aspernatur.
-                    </p>
-                    <div className="flex items-center justify-around">
-                        <button
-                            className={clsx(
-                                "bg-slate-200 hover:bg-slate-300 transition text-slate-950",
-                                "flex items-center justify-center",
-                                "py-2 px-4 cursor-pointer rounded-lg",
-                            )}
-                            autoFocus
-                        >
-                            Cancelar
-                        </button>
-                        <button
-                            className={clsx(
-                                "bg-blue-500 hover:bg-blue-600 transition text-blue-50",
-                                "flex items-center justify-center",
-                                "py-2 px-4 cursor-pointer rounded-lg",
-                            )}
-                        >
-                            Ok
-                        </button>
-                    </div>
-                </div>
-            </div>
         </div>
     );
 }
