0680d55026f4c4ceb47abfc43e0211802b023957
André Luiz <andreluizoliveira163@hotmail.com>
2026-02-03 10:03:39 -0400

    Criando o componente para InputCheckbox

--- src/app/admin/post/new/page.tsx
+++ src/app/admin/post/new/page.tsx
@@ -1,43 +1,50 @@
 import { Button } from "@/components/Button";
+import { InputCheckbox } from "@/components/InputCheckbox";
 import { InputText } from "@/components/InputText";
-import { BanIcon, BugIcon, CheckIcon } from "lucide-react";
 
 export const dynamic = "force-dynamic";
 
 export default async function AdminPostNewPage() {
     return (
-        <div className="flex flex-col gap-6">
-            <InputText
-                labelText="Nome"
-                placeholder="Digite seu nome"
-                type="password"
-            />
-            <InputText
-                labelText="Sobrenome"
-                placeholder="Digite seu sobrenome"
-            />
-            <InputText
-                labelText="Sobrenome"
-                placeholder="Digite seu sobrenome"
-                defaultValue={"Olá mundo"}
-                disabled
-            />
-            <InputText
-                labelText="Sobrenome"
-                placeholder="Digite seu sobrenome"
-                disabled
-            />
-            <InputText
-                labelText="Sobrenome"
-                placeholder="Digite seu sobrenome"
-                readOnly
-            />
-            <InputText
-                labelText="Sobrenome"
-                placeholder="Digite seu sobrenome"
-                defaultValue={"Olá mundo"}
-                readOnly
-            />
-        </div>
+        <form action="" className="mb-16">
+            <div className="flex flex-col gap-6">
+                <InputText
+                    labelText="Nome"
+                    placeholder="Digite seu nome"
+                    type="password"
+                />
+                <InputText
+                    labelText="Sobrenome"
+                    placeholder="Digite seu sobrenome"
+                />
+                <InputCheckbox labelText="Sobrenome" />
+                <InputText
+                    labelText="Sobrenome"
+                    placeholder="Digite seu sobrenome"
+                    defaultValue={"Olá mundo"}
+                    disabled
+                />
+                <InputText
+                    labelText="Sobrenome"
+                    placeholder="Digite seu sobrenome"
+                    disabled
+                />
+                <InputText
+                    labelText="Sobrenome"
+                    placeholder="Digite seu sobrenome"
+                    readOnly
+                />
+                <InputText
+                    labelText="Sobrenome"
+                    placeholder="Digite seu sobrenome"
+                    defaultValue={"Olá mundo"}
+                    readOnly
+                />
+
+                <div className="mt-4">
+                    <Button type="submit">Enviar</Button>
+                </div>
+            </div>
+        </form>
     );
 }
